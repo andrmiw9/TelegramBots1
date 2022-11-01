a08c496d2c46906a93bb7bf0f5f5f8a8f051fddf
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-11-01 19:55:15 +0300

    End?

--- Aiogram.py
+++ Aiogram.py
@@ -1,25 +1,56 @@
+import os
+
 from aiogram import Bot, Dispatcher, executor, types
 import time
 import logging
-import asyncio
-from config import TOKEN as API_TOKEN
+from asyncio import sleep
+from dotenv import load_dotenv
 
-bot = Bot(API_TOKEN)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+res = load_dotenv('venv\.env')
+if res:
+    print('yep')
+else:
+    print('no')
+bot = Bot(os.environ['BOT_TOKEN'])
 dp = Dispatcher(bot)
-MSG = "Здарова мухомор {}!"
+MSG = "Хочешь скипнуть пары, но не уверен?) Тогда отправь мне /dice и мы решим это вместе, {}!"
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'help'])
 async def start_handler(message: types.Message):
+    print(message)
+    if (message.text == '/start'):
+        await message.reply(f"Привет, {message.from_user.full_name}!")
+
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+    print(f'{user_id} {user_full_name} {time.asctime()}')
 
-    await message.reply(f"Привет, {user_full_name}!")
-
-    await asyncio.sleep(1)
     await bot.send_message(user_id, MSG.format(user_name))
+
+
+@dp.message_handler(commands=['dice'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    bot_data = await bot.send_dice(user_id)
+    # print(bot_data['dice']['value'])
+    bot_data = bot_data['dice']['value']
+
+    await sleep(1)
+    user_data = await bot.send_dice(user_id)
+    user_data = user_data['dice']['value']
+
+    await sleep(1)
+    if bot_data > user_data:
+        await bot.send_message(user_id, 'ИДИ НА ПАРЫ, ЛУЗЕР!')
+    else:
+        await bot.send_message(user_id, 'ЮХУУУУ ВАЛИМ НАХЕР!')
+    pass
 
 
 if __name__ == '__main__':
